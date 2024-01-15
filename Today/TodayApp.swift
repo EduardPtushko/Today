5e59a8362d5a830c5ad4f2cffeb6dfbd121ac059
Эдуард Птушко <ptushko@gmail.com>
2024-01-15 19:27:55 +0300

    Initial Commit

---
+++ Today/TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayApp.swift
+//  Today
+//
+//  Created by Eduard Ptushko on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
